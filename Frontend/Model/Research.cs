854f4cc3f3fd28ea6cd267ab88b680d8a3324f1d
Shirin Meirkhan <sm223qi@student.lnu.se>
2024-01-30 14:23:13 +0100

    create frontend with blazorserver

---
+++ Frontend/Model/Research.cs
@@ -0,0 +1,16 @@
+namespace Frontend
+{
+
+
+  public class Research
+  {
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public string ImageUrl { get; set; }
+
+  }
+}
