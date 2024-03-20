2e3abfdb5b0ea9e60f702d0a79d7522dbdfb14fd
Shirin Meirkhan <sm223qi@student.lnu.se>
2024-03-20 14:34:42 +0100

    Separate c# code

---
+++ Frontend/Pages/AiAssistant.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Frontend.Models;
+using Frontend.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace Frontend.Pages
+{
+    public partial class  AiAssistant : ComponentBase
+    {
+        [Inject]
+        public ChatService? ChatService {get; set;}
+
+        private string userQuery = "";
+        private List<ChatMessage> chatHistory = new List<ChatMessage>();
+        private bool isChatHistoryLoaded = false;
+        private object chat = new { };
+
+        protected override async Task OnInitializedAsync()
+        {
+            var chatResponse = await ChatService.GetChatHistoryAsync();
+            if (chatResponse?.Messages != null)
+            {
+                chatHistory = chatResponse.Messages.OrderBy(m => m.CreatedAt).ToList();
+            }
+            isChatHistoryLoaded = true; // Indicate that the chat history has been loaded
+        }
+
+        private async Task HandleSubmitAsync()
+        {
+            var chatRequest = new ChatRequest { UserMessage = userQuery };
+            var chatResponse = await ChatService.PostUserQueryAsync(chatRequest);
+
+            if (chatResponse?.Messages != null)
+            {
+                chatHistory.AddRange(chatResponse.Messages);
+                chatHistory = chatHistory.OrderBy(m => m.CreatedAt).ToList();
+            }
+            userQuery = "";
+        }
+    }
+}
