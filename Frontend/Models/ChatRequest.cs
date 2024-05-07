182cc657d1f1e42c4e582f2393ef7b0699929f79
Shirin Meirkhan <sm223qi@student.lnu.se>
2024-05-07 08:26:26 +0200

    Prevent the user from sending an empty message

--- Frontend/Models/ChatRequest.cs
+++ Frontend/Models/ChatRequest.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,8 @@ namespace Frontend.Models
 {
    public class ChatRequest
 {
+    [Required(ErrorMessage = "You must type a message before sending.")]
+    [MinLength(1, ErrorMessage = "The message cannot be empty.")]
     public string? UserMessage { get; set; }
 }
 
